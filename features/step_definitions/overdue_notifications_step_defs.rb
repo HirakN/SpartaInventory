8b06af687d05b4554991abeb90be329160910127
LukeH1993 <lwheudebourck@gmail.com>
2017-11-21 15:37:04 +0000

    completed most of overdue notifications step defs

--- features/step_definitions/overdue_notifications_step_defs.rb
+++ features/step_definitions/overdue_notifications_step_defs.rb
@@ -1,20 +1,24 @@
 Given("that I have checked out an item") do
-  pending # Write code here that turns the phrase above into concrete actions
+  login_page.visit_login_page
+  login_page.fill_in_email_field
+  login_page.fill_in_password_field
+  login_page.click_submit_button
+  items.url
+  item_show_page.visit_page
 end
 
 And("the item has not been returned") do
-  pending # Write code here that turns the phrase above into concrete actions
-
+  pending
 end
 
 And("that I have set a date for its return") do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending
 end
 
 When("that date is reached") do
-  pending # Write code here that turns the phrase above into concrete actions
+  item_show_page.find_due_date
 end
 
 Then("I expect the lender to receive a notification") do
-  pending # Write code here that turns the phrase above into concrete actions
+  item_show_page.find_success_notification
 end
