31dbc448762430419c1a91d2864b1b8629e8e43b
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-11-21 12:11:10 +0000

    edits

--- features/description.feature
+++ features/description.feature
@@ -5,7 +5,7 @@ I expect all items to have a non-empty description
 
 Scenario: All item descriptions should be filled in
   Given that I am logged in
-  And I am on the create new items page
+  And Im on the create new items page
   When I leave an empty description
   And I click save item
   Then I see an error message
--- features/step_definitions/description_step_defs.rb
+++ features/step_definitions/description_step_defs.rb
@@ -1,19 +1,22 @@
 Given("that I am logged in") do
-  checkout_item.fill_in_email_field
-  checkout_item.fill_in_password_field
-
+  login_page.visit_login_page
+  login_page.fill_in_email_field
+  login_page.fill_in_password_field
+  login_page.click_submit_button
 end
 
-Given("I am on the create new items page") do
-  pending # Write code here that turns the phrase above into concrete actions
+And("Im on the create new items page") do
+  sleep 5
+  navbar.click_items_link
+  items.click_add_new_item_button
+
 end
 
 When("I leave an empty description") do
-
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("I click save item") do
+And("I click save item") do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -21,6 +24,6 @@ Then("I see an error message") do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("no record is created") do
+And("no record is created") do
   pending # Write code here that turns the phrase above into concrete actions
 end
