b6ea25a83776cadd458bbaebd31ff37387f508fd
Tech-A67 <tech-a67@Tech-A67s-MacBook-Air.local>
2018-01-22 15:03:57 +0000

    Replaced the id with serial number in the items index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :check_login
+  # before_action :check_login
 
   # GET /items
   # GET /items.json
