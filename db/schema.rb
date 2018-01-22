e2f76cd90d515cbefe8a7e1972672064eaa6b546
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-01-22 16:12:17 +0000

    added notes functionality to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122144536) do
+ActiveRecord::Schema.define(version: 20180122160622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
