65479c02a865a6988f9fa2712f03111010d66f33
Tech-A33 <Tech-A33@Tech-A33s-MacBook-Air.local>
2018-01-22 15:53:36 +0000

    added notes text box

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906092538) do
+ActiveRecord::Schema.define(version: 20180122144536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170906092538) do
     t.datetime "updated_at",  null: false
     t.text     "serial"
     t.integer  "academy_id"
+    t.text     "notes"
   end
 
   create_table "logs", force: :cascade do |t|
